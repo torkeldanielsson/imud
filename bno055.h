367125efebf66150a4c76d8eabcba7eb9cf7e456
Torkel Danielsson <torkel.danielsson@gmail.com>
2015-05-21 21:30:24 +0200

    first

---
+++ bno055.h
@@ -0,0 +1,33 @@
+#ifndef BNO_055_H
+#define BNO_055_H
+
+#define BNO055_ADDRESS_A                  (0x28)
+#define BNO055_ADDRESS_B                  (0x29)
+#define BNO055_ID                         (0xA0)
+
+#define BNO055_CHIP_ID_ADDR               (0x00)
+
+#define BNO055_PAGE_ID_ADDR               (0x07)
+
+#define OPERATION_MODE_CONFIG             (0x00)
+#define OPERATION_MODE_NDOF               (0x0C)
+
+#define POWER_MODE_NORMAL                 (0x00)
+
+#define BNO055_SYS_TRIGGER_ADDR           (0x3F)
+
+#define BNO055_OPR_MODE_ADDR              (0x3D)
+#define BNO055_PWR_MODE_ADDR              (0x3E)
+
+#define BNO055_QUATERNION_DATA_W_LSB_ADDR (0x20)
+#define BNO055_QUATERNION_DATA_W_MSB_ADDR (0x21)
+#define BNO055_QUATERNION_DATA_X_LSB_ADDR (0x22)
+#define BNO055_QUATERNION_DATA_X_MSB_ADDR (0x23)
+#define BNO055_QUATERNION_DATA_Y_LSB_ADDR (0x24)
+#define BNO055_QUATERNION_DATA_Y_MSB_ADDR (0x25)
+#define BNO055_QUATERNION_DATA_Z_LSB_ADDR (0x26)
+#define BNO055_QUATERNION_DATA_Z_MSB_ADDR (0x27)
+
+#define BNO055_TEMP_ADDR                  (0x34)
+
+#endif /* BNO_055_H */
